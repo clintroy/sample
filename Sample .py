4d78821a814aeee13d12af8b44e69c7d2cf54430
clintroy <clint.roy4690@gmail.com>
2017-11-13 12:35:53 +0530

    Create Sample .py

---
+++ Sample .py
@@ -0,0 +1,14 @@
+# sample
+#!/usr/bin/python
+print "How old are you?",
+age = raw_input()
+print  "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+ print "So you're %r old, %r tall and %r heavy." % ( age, height, weight)
+ 
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input(" How much you weight? ")
+ print "So you are %r old, %r tall and %r heavy." % (age, height, weight)
